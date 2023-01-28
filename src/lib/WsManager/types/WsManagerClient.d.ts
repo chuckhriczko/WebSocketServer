28aa6e0a943f675cd55f280e9ff37eade822015f
Chuck Hriczko <chriczko@libertyfoxtech.com>
2023-01-27 20:01:17 -0500

    Fixed build errors

--- src/lib/WsManager/types/WsManagerClient.d.ts
+++ src/lib/WsManager/types/WsManagerClient.d.ts
@@ -1,10 +1,8 @@
 import { SocketStream } from "@fastify/websocket";
-import User from "../../../data/entities/User";
 
 export default interface WsManagerClient{
     client: SocketStream; //WebSocket client object
     clientId: number | string; //User ID or UUID if user is not authenticated
     ip: string; //IP of the connected user
-    user: User | null; //User object, if authenticated
     isAuthenticated: boolean; //Flag determining if user is authenticated
 }
